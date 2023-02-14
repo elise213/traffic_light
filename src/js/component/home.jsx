b11eba4f6940a070bd4e28ca22292d8261da46d5
xfontainex <maralafontaine@gmail.com>
2023-02-14 18:52:56 +0000

    I made it so that the color of the glow matches the color of the light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
       <div>
         <div className="trafficTop"></div>
         <div className="trafficLight">
-          <div className={"light red" + (color === "red" ? " glow": "")} onClick={() => {setColor("red")}}></div>
-          <div className={"light yellow" + (color === "yellow" ? " glow": "")} onClick={() => {setColor("yellow")}}></div>
-          <div className={"light green" + (color === "green" ? " glow": "")} onClick={() => {setColor("green")}}></div>
+          <div className={"light red" + (color === "red" ? " glow-red": "")} onClick={() => {setColor("red")}}></div>
+          <div className={"light yellow" + (color === "yellow" ? " glow-yellow": "")} onClick={() => {setColor("yellow")}}></div>
+          <div className={"light green" + (color === "green" ? " glow-green": "")} onClick={() => {setColor("green")}}></div>
         </div>
       </div>
     </div>
